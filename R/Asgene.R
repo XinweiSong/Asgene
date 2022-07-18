e495a530ee91e37233614c92eb85b515b0da4bf9
XinweiSong <75347824+XinweiSong@users.noreply.github.com>
2022-07-18 20:56:37 +0800

    Update Asgene.R

--- R/Asgene.R
+++ R/Asgene.R
@@ -1,4 +1,4 @@
-#' AsgeneDB: A functional gene database for metagenomic profiling of arsenic metabolism
+#' AsgeneDB: A functional gene database for metagenomic profiling of arsenic metabolism#' AsgeneDB: A functional gene database for metagenomic profiling of arsenic metabolism
 #' @description A manually curated arsenic functional gene database (AsgeneDB) and R package (Asgene package) are developed for rapid and accurate metagenomic analysis.
 #' @param analysis Choose the target you want to analyze,abundance or taxonomy; type = "character"; default = "abundance"
 #' @param workdir Specify directory for sequence file location;type = "character"; default = "./"
